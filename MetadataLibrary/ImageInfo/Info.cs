4b533b3bf7118beb9332ee0ef53e567dd1794e3a
NII IKT <office@nii-ikt.ru>
2016-03-04 22:59:47 +0600

    Библиотека для работы с метаданными изображений

---
+++ MetadataLibrary/ImageInfo/Info.cs
@@ -0,0 +1,487 @@
+﻿//http://www.codeproject.com/Articles/7888/A-library-to-simplify-access-to-image-metadata
+
+using System;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Collections;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace MetadataLibrary.ImageInfo
+{
+    ///<summary>This Class retrives Image Properties using Image.GetPropertyItem() method 
+    /// and gives access to some of them trough its public properties. Or to all of them
+    /// trough its public property PropertyItems.
+    ///</summary>
+    public class Info
+    {
+        ///<summary>Wenn using this constructor the Image property must be set before accessing properties.</summary>
+        public Info()
+        {
+        }
+
+        ///<summary>Creates Info Class to read properties of an Image given from a file.</summary>
+        /// <param name="imageFileName">A string specifiing image file name on a file system.</param>
+        public Info(string imageFileName)
+        {
+            _image = System.Drawing.Image.FromFile(imageFileName);
+        }
+
+        ///<summary>Creates Info Class to read properties of a given Image object.</summary>
+        /// <param name="anImage">An Image object to analise.</param>
+        public Info(System.Drawing.Image anImage)
+        {
+            _image = anImage;
+        }
+
+        System.Drawing.Image _image;
+        ///<summary>Sets or returns the current Image object.</summary>
+        public System.Drawing.Image Image
+        {
+            set { _image = value; }
+            get { return _image; }
+        }
+
+        ///<summary>
+        /// Type is PropertyTagTypeShort or PropertyTagTypeLong
+        ///Information specific to compressed data. When a compressed file is recorded, the valid width of the meaningful image must be recorded in this tag, whether or not there is padding data or a restart marker. This tag should not exist in an uncompressed file.
+        /// </summary>
+        public uint? PixXDim
+        {
+            get
+            {
+                try
+                {
+                    object tmpValue = PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.ExifPixXDim));
+                    if (tmpValue.GetType().ToString().Equals("System.UInt16")) return (uint)(ushort)tmpValue;
+                    return (uint)tmpValue;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+        ///<summary>
+        /// Type is PropertyTagTypeShort or PropertyTagTypeLong
+        /// Information specific to compressed data. When a compressed file is recorded, the valid height of the meaningful image must be recorded in this tag whether or not there is padding data or a restart marker. This tag should not exist in an uncompressed file. Because data padding is unnecessary in the vertical direction, the number of lines recorded in this valid image height tag will be the same as that recorded in the SOF.
+        /// </summary>
+        public uint? PixYDim
+        {
+            get
+            {
+                try
+                {
+                    object tmpValue = PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.ExifPixYDim));
+                    if (tmpValue.GetType().ToString().Equals("System.UInt16")) return (uint)(ushort)tmpValue;
+                    return (uint)tmpValue;
+                }               
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        ///<summary>
+        ///Number of pixels per unit in the image width (x) direction. The unit is specified by PropertyTagResolutionUnit
+        ///</summary>
+        public Fraction XResolution
+        {
+            get
+            {
+                try
+                {
+                    return (Fraction)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.XResolution));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        ///<summary>
+        ///Number of pixels per unit in the image height (y) direction. The unit is specified by PropertyTagResolutionUnit.
+        ///</summary>
+        public Fraction YResolution
+        {
+            get
+            {
+                try
+                {
+                    return (Fraction)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.YResolution));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        ///<summary>
+        ///Unit of measure for the horizontal resolution and the vertical resolution.
+        ///2 - inch 3 - centimeter
+        ///</summary>
+        public ResolutionUnit? ResolutionUnit
+        {
+            get
+            {
+                try
+                {
+                    return (ResolutionUnit)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.ResolutionUnit));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        ///<summary>
+        ///Brightness value. The unit is the APEX value. Ordinarily it is given in the range of -99.99 to 99.99.
+        ///</summary>
+        public Fraction Brightness
+        {
+            get
+            {
+                try
+                {
+                    return (Fraction)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.ExifBrightness));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        ///<summary>
+        /// The manufacturer of the equipment used to record the image.
+        ///</summary>
+        public string EquipMake
+        {
+            get
+            {
+                try
+                { 
+                return (string)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.EquipMake));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        ///<summary>
+        /// The model name or model number of the equipment used to record the image.
+        /// </summary>
+        public string EquipModel
+        {
+            get
+            {
+                try
+                { 
+                return (string)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.EquipModel));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        ///<summary>
+        ///Copyright information.
+        ///</summary>
+        public string Copyright
+        {
+            get
+            {
+                try
+                { 
+                return (string)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.Copyright));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            set
+            {
+                SetValue(value, (int)PropertyTagId.Copyright);
+            }
+        }
+
+
+        ///<summary>
+        ///Date and time the image was created.
+        ///</summary>		
+        public string DateTime
+        {
+            get
+            {
+                try
+                { 
+                return (string)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.DateTime));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        //The format is YYYY:MM:DD HH:MM:SS with time shown in 24-hour format and the date and time separated by one blank character (0x2000). The character string length is 20 bytes including the NULL terminator. When the field is empty, it is treated as unknown.
+        private static DateTime ExifDTToDateTime(string exifDT)
+        {
+            exifDT = exifDT.Replace(' ', ':');
+            string[] ymdhms = exifDT.Split(':');
+            int years = int.Parse(ymdhms[0]);
+            int months = int.Parse(ymdhms[1]);
+            int days = int.Parse(ymdhms[2]);
+            int hours = int.Parse(ymdhms[3]);
+            int minutes = int.Parse(ymdhms[4]);
+            int seconds = int.Parse(ymdhms[5]);
+            return new DateTime(years, months, days, hours, minutes, seconds);
+        }
+
+        ///<summary>
+        ///Date and time when the original image data was generated. For a DSC, the date and time when the picture was taken. 
+        ///</summary>
+        public DateTime? DTOrig
+        {
+            get
+            {
+                try
+                {
+                    string tmpStr = (string)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.ExifDTOrig));
+                    return ExifDTToDateTime(tmpStr);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        ///<summary>
+        ///Date and time when the image was stored as digital data. If, for example, an image was captured by DSC and at the same time the file was recorded, then DateTimeOriginal and DateTimeDigitized will have the same contents.
+        ///</summary>
+        public DateTime? DTDigitized
+        {
+            get
+            {
+                try
+                {
+                    string tmpStr = (string)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.ExifDTDigitized));
+                    return ExifDTToDateTime(tmpStr);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+
+        ///<summary>
+        ///ISO speed and ISO latitude of the camera or input device as specified in ISO 12232.
+        ///</summary>		
+        public ushort? ISOSpeed
+        {
+            get
+            {
+                try
+                { 
+                return (ushort)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.ExifISOSpeed));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            //TODO реализоовать
+            //set
+            //{
+            //    SetValue(value, (int)PropertyTagId.ExifISOSpeed);
+            //}
+        }
+
+        ///<summary>
+        ///Image orientation viewed in terms of rows and columns.
+        ///</summary>				
+        public Orientation? Orientation
+        {
+            get
+            {
+                try
+                {
+                    return (Orientation)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.Orientation));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        ///<summary>
+        ///Actual focal length, in millimeters, of the lens. Conversion is not made to the focal length of a 35 millimeter film camera.
+        ///</summary>						
+        public Fraction FocalLength
+        {
+            get
+            {
+                try
+                {
+                    return (Fraction)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.ExifFocalLength));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        ///<summary>
+        ///F number.
+        ///</summary>						
+        public Fraction FNumber
+        {
+            get
+            {
+                try
+                {
+                    return (Fraction)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.ExifFNumber));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        ///<summary>
+        ///Class of the program used by the camera to set exposure when the picture is taken.
+        ///</summary>						
+        public ExposureProg? ExposureProg
+        {
+            get
+            {
+                try
+                {
+                    return (ExposureProg)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.ExifExposureProg));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        ///<summary>
+        ///Metering mode.
+        ///</summary>						
+        public MeteringMode? MeteringMode
+        {
+            get
+            {
+                try
+                {
+                    return (MeteringMode)PropertyTag.getValue(_image.GetPropertyItem((int)PropertyTagId.ExifMeteringMode));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        private Hashtable _propertyItems;
+        ///<summary>
+        /// Returns a Hashtable of all available Properties of a gieven Image. Keys of this Hashtable are
+        /// Display names of the Property Tags and values are transformed (typed) data.
+        ///</summary>
+        /// <example>
+        /// <code>
+        /// if (openFileDialog.ShowDialog()==DialogResult.OK)
+        ///	{
+        ///		Info inf=new Info(Image.FromFile(openFileDialog.FileName));
+        ///		listView.Items.Clear();
+        ///		foreach (string propertyname in inf.PropertyItems.Keys)
+        ///		{
+        ///			ListViewItem item1 = new ListViewItem(propertyname,0);
+        ///		    item1.SubItems.Add((inf.PropertyItems[propertyname]).ToString());
+        ///			listView.Items.Add(item1);
+        ///		}
+        ///	}
+        /// </code>
+        ///</example>
+        public Hashtable PropertyItems
+        {
+            get
+            {
+                if (_propertyItems == null)
+                {
+                    _propertyItems = new Hashtable();
+                    foreach (int id in _image.PropertyIdList)
+                        _propertyItems[((PropertyTagId)id).ToString()] = PropertyTag.getValue(_image.GetPropertyItem(id));
+
+                }
+                return _propertyItems;
+            }
+        }
+
+        public void SetValue(string value, int id)
+        {
+            PropertyItem pi; //получаем элемент
+            try
+            {
+                pi = _image.GetPropertyItem(id); //элемент либо есть
+            }
+            catch
+            {
+                pi = (PropertyItem)FormatterServices.GetUninitializedObject(typeof(PropertyItem)); //либо его нет и мы его создаем
+                pi.Type = PropertyTag.GetTypeFromId(id);
+            }
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+           
+            char[] str= value.ToString().ToCharArray();
+            byte[] val = Encoding.ASCII.GetBytes(str);
+            //encoding.GetString(val, 0, val.Length);
+
+            pi.Value = val;
+            pi.Len = pi.Value.Length + 1;
+            _image.SetPropertyItem(pi);//запись в image
+        }
+
+        public void SetValue(short value, int id)
+        {
+            PropertyItem pi; //получаем элемент
+            try
+            {
+                pi = _image.GetPropertyItem(id); //элемент либо есть
+            }
+            catch
+            {
+                pi = (PropertyItem)FormatterServices.GetUninitializedObject(typeof(PropertyItem)); //либо его нет и мы его создаем
+                pi.Type = PropertyTag.GetTypeFromId(id);
+            }
+               //case PropertyTagType.Short:
+               //     ushort[] _resultUShort = new ushort[aPropertyItem.Len / (16 / 8)];
+               //     for (int i = 0; i < _resultUShort.Length; i++)
+               //         _resultUShort[i] = BitConverter.ToUInt16(aPropertyItem.Value, i * (16 / 8));
+               //     if (_resultUShort.Length == 1) return _resultUShort[0];
+               //     return (_resultUShort);
+
+            byte[] val= BitConverter.GetBytes(value);
+
+            pi.Value = val;
+            pi.Len = pi.Value.Length + 1;
+            _image.SetPropertyItem(pi);//запись в image
+        }
+    }
+}
---
+++ MetadataLibrary/MetaData.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Collections;
+
+namespace MetadataLibrary
+{
+    /// <summary>
+    /// Класс для работы с метаданными картинок
+    /// </summary>
+    public class MetaData
+    {
+        /// <summary>
+        /// Метод который удаляет все метаданные из файла картинки
+        /// </summary>
+        /// <param name="pathFile">Путь до файла</param>
+        /// <returns>Истина - удаление прошло успешно, ЛОЖЬ - что то пошло не так</returns>
+        public bool Remove(string pathFile) //TODO в этом коде нет try cath, нужно потестить
+        {
+
+            Image sourceImg = new Bitmap(pathFile);
+            Image img = (Image)sourceImg.Clone();
+            sourceImg.Dispose();
+
+            PropertyItem[] propertyItemsList = img.PropertyItems;
+            //к сожалниею PropertyItems доступен только для чтения а операции PropertyItems.Clear() нет, поэтому приходится удалять вручную, все так делают
+            foreach (PropertyItem property in propertyItemsList)
+            {
+                img.RemovePropertyItem(property.Id);
+            }
+
+            string mime = GetMimeType(pathFile);
+            ImageCodecInfo Encoder = GetEncoderInfo(mime);
+            EncoderParameters EncoderParams = new EncoderParameters(2);
+            EncoderParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression, (Int64)EncoderValue.CompressionNone);//TODO решить со сжатием
+            EncoderParams.Param[1] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 100L);
+            
+            //string newfile = openFileDialog1.FileName.Replace(openFileDialog1.SafeFileName, "") + "Clear_" + openFileDialog1.SafeFileName;
+            //if (System.IO.File.Exists(newfile))
+            //{
+            //    System.IO.File.Delete(newfile);
+            //}
+
+            //для удаоние файла нужно удалить img а в енм хранится картинка
+            
+            //HACK болт, но иначе не работает нашел здесь: http://stackoverflow.com/questions/19460149/c-sharp-remove-property-tag-items-from-tif-file 
+            img.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            img.RotateFlip(RotateFlipType.Rotate180FlipNone);
+
+            img.Save(pathFile+".tmp", Encoder, EncoderParams);
+            img.Dispose();
+
+            //к сожалению, после освобождения объекта sourceImg через Dispose файл pathFile нельзя удалить.
+            //Операция sourceImg.Clone не делает клона, а передает ссылку на файл
+            //файл pathFile можно удалить только после того как img высвободить черещ Dispose
+            //Но тогда нечего будет сохранять
+            //тупое решение - записать в другой файл, потом удалить оригинал и переименовать.
+
+            File.Delete(pathFile);
+            File.Move(pathFile + ".tmp", pathFile);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Метод который удаляет все метаданные из файла картинки
+        /// </summary>
+        /// <param name="metadataPathFile">Путь до файла хранящего метаданные</param>
+        /// <param name="imagePathFile">Путь до файла хранящего изображения</param>
+        /// <returns>Истина - копирование прошло успешно, ЛОЖЬ - что то пошло не так</returns>
+        public bool Copy(string metadataPathFile, string imagePathFile)
+        {
+
+            Image sourceImg = new Bitmap(metadataPathFile);
+            Image img = new Bitmap(imagePathFile);
+
+            //очищаем изображение от мета
+            PropertyItem[] propertyItemsList = img.PropertyItems;
+            //к сожалниею PropertyItems доступен только для чтения а операции PropertyItems.Clear() нет, поэтому приходится удалять вручную, все так делают
+            foreach (PropertyItem property in propertyItemsList)
+            {
+                img.RemovePropertyItem(property.Id);
+            }
+
+            //заполняем метаданными от старого рисунка
+            propertyItemsList = sourceImg.PropertyItems;
+            foreach (PropertyItem property in propertyItemsList)
+            {
+                img.SetPropertyItem(property);
+            }
+
+            string mime = GetMimeType(imagePathFile);
+            ImageCodecInfo Encoder = GetEncoderInfo(mime);
+            EncoderParameters EncoderParams = new EncoderParameters(2);
+            EncoderParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression, (Int64)EncoderValue.CompressionNone);//TODO решить со сжатием
+            EncoderParams.Param[1] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 100L);
+
+            //string newfile = openFileDialog2.FileName.Replace(openFileDialog2.SafeFileName, "") + "New_" + openFileDialog2.SafeFileName;
+            //if (System.IO.File.Exists(newfile))
+            //{
+            //    System.IO.File.Delete(newfile);
+            //}
+
+            //TODO болт, но иначе не работает нашел здесь: http://stackoverflow.com/questions/19460149/c-sharp-remove-property-tag-items-from-tif-file 
+            img.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            img.RotateFlip(RotateFlipType.Rotate180FlipNone);
+
+            img.Save(imagePathFile + ".tmp", Encoder, EncoderParams);
+
+            EncoderParams.Dispose();
+            img.Dispose();
+            sourceImg.Dispose();
+
+            File.Delete(imagePathFile);
+            File.Move(imagePathFile + ".tmp", imagePathFile);
+
+            return true;
+        }
+
+        public Hashtable Read(string pathFile)//TODO нехороший метод
+        {
+            //MetadataLibrary.ImageInfo.Info info = new ImageInfo.Info(pathFile);
+            // info.Image.Dispose();
+            // return info.PropertyItems; //HACK вот здесб жопа
+            return new Hashtable();
+        }
+
+        public Dictionary<string,string> ReadAll(string pathFile)//TODO нехороший метод
+        {
+            Dictionary<string, string> res = new Dictionary<string, string>();
+            MetadataLibrary.ImageInfo.Info info = new ImageInfo.Info(pathFile);
+            res.Add("PixXDim", info.PixXDim == 1123456789 ? "n/a" : info.PixXDim.ToString());
+            res.Add("PixYDim", info.PixYDim == 1123456789? "n/a" :info.PixYDim.ToString());
+            res.Add("EquipModel", String.Copy(info.EquipModel));
+            res.Add("EquipMake", String.Copy(info.EquipMake));
+            res.Add("Copyright", String.Copy(info.Copyright));
+            res.Add("DateTime", String.Copy(info.DateTime));
+            res.Add("ISOSpeed", info.ISOSpeed == 12345 ? "n/a" : info.ISOSpeed.ToString());
+            info.Image.Dispose();
+            return res;
+        }
+
+        public MetadataLibrary.ImageInfo.Info AllTestRead(string pathFile)
+        {
+            MetadataLibrary.ImageInfo.Info info = new ImageInfo.Info(pathFile);
+            return info;
+        }
+
+        public bool Write(string pathFile)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Возвращает mime файла в типе стринг основываюсь на расширение файла
+        /// </summary>
+        /// <param name="pathFileImg">Путь до файла</param>
+        /// <returns>Тип mime в типе стринг</returns>
+        private string GetMimeType(string pathFileImg)
+        {            
+            var file = Path.GetFileName(pathFileImg).ToLower().Split('.');
+            switch (file[file.LongLength - 1])
+            {
+                case "gif": return "image/gif";
+                case "png": return "image/png";                
+                case "bmp": return "image/bmp";                
+                case "dib": return "image/bmp";
+                case "jpe": return "image/jpeg";                 
+                case "jpg": return "image/jpeg";                 
+                case "jpeg":return "image/jpeg";                  
+                case "jfif": return "image/jpeg";           
+                case "tif": return "image/tiff";                  
+                case "tiff": return "image/tiff";                  
+                default: return "ups_not_mime_type"; 
+            }
+        }
+
+        /// <summary>
+        /// Возвращает кодировку изображения (GDI+)
+        /// </summary>
+        /// <param name="mimeType">Название кодировки в текстовом виде</param>
+        /// <returns>Кодировка изображения</returns>
+        private ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            // Получить кодеки изображения для всех форматов изображений
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            // Найти нужный кодек изображения
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+            return null;
+        }
+
+    }
+}
